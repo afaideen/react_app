db56c0db0815ab1d5cbd01dfb822b4eb53d3340e
han <afaideen@gmail.com>
2024-06-15 07:25:09 +0800

    add booklist

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function Greeting() {
-    return (<h2>My First Component</h2>)
+import './index.css';
+// function Greeting() {
+//     return (<h2>My First Component</h2>)
     
-}
+// }
+
+import { books } from './books.js';
+import Book from './Book';
+function BookList() {
+    return (
+      <>
+        <h1>amazon best sellers</h1>
+        <section className='booklist'>
+          {books.map((book) => {
+            return <Book {...book} key={book.id} />;
+          })}
+        </section>
+      </>
+    );
+  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting/>)
+// root.render(<Greeting/>)
+root.render(<BookList />);
