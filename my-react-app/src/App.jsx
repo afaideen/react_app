f9308a86b477b5cdc82cc40853a961f1f7f145be
han <afaideen@gmail.com>
2024-06-26 13:02:58 +0800

    react hook advanced

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import './index.css'
+// import ErrorExample from "./01-error-example.jsx";
+// import UseStateBasics from "./02-useState-basics.jsx";
+// import UseStateArray from "./03-useState-array.jsx";
+// import UseStateObject from './04-useState-object.jsx'
+import UseStateGotcha from './05-useState-gotcha.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+          <h2>Advanced React</h2>
+          <UseStateGotcha />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
